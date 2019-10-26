859a34556e956ee2054e16a9587c807b219a3f76
zayashnikovre <un0blogger@gmail.com>
2019-10-26 23:22:17 +0300

    cors2

--- app.py
+++ app.py
@@ -13,11 +13,5 @@ def predict():
         'prediction': prediction
     })
 
-@blueprint.after_request # blueprint can also be app~~
-def after_request(response):
-    header = response.headers
-    header['Access-Control-Allow-Origin'] = '*'
-    return response
-
 if __name__ == "__main__":
     app.run(debug=True)
