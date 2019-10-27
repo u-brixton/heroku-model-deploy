ca0e9ddeffea70e983de784c4105b7197ef5614d
zayashnikovre <un0blogger@gmail.com>
2019-10-27 04:00:20 +0300

    AA

--- app.py
+++ app.py
@@ -34,9 +34,6 @@ else:
 
 class Prediction(Model):
     observation_id = IntegerField(unique=True)
-    observation = TextField()
-    proba = FloatField()
-    true_class = IntegerField(null=True)
 
     class Meta:
         database = DB
@@ -44,18 +41,6 @@ class Prediction(Model):
 
 DB.create_tables([Prediction], safe=True)
 
-
-class answers_short(Model):
-    observation_id = IntegerField(unique=True)
-    observation = TextField()
-    proba = FloatField()
-    true_class = IntegerField(null=True)
-
-    class Meta:
-        database = DB
-
-
-DB.create_tables([answers_short], safe=True)
 
 # End database stuff
 ########################################
@@ -127,7 +112,7 @@ def update():
 @app.route('/list-db-contents')
 def list_db_contents():
     return jsonify([
-        model_to_dict(obs) for obs in answers_short.select()
+        model_to_dict(obs) for obs in Prediction.select()
     ])
 
 
